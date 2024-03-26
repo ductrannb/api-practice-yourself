98cc2de01cdbf5ce5187971338424f2b825ed512
Tran Xuan Duc <xduc24122002@gmail.com>
2024-03-26 14:02:42 +0700

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123123'),
+            'name' => 'Admin',
+            'role_id' => User::ROLE_ADMIN
+        ]);
+    }
+}
