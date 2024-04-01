a582a0455d3efcdf059a913bc2a061f0edf1549f
Tran Xuan Duc <xduc24122002@gmail.com>
2024-04-01 17:02:06 +0700

    add middleware

---
+++ app/Http/Middleware/Teacher.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Teacher
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->check()) {
+            return response()->json(['message' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+        }
+        if (auth()->user()->role_id !== User::ROLE_TEACHER && auth()->user()->role_id !== User::ROLE_ADMIN) {
+            return response()->json(['message' => 'You do not have access'], Response::HTTP_FORBIDDEN);
+        }
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('api')->group(function () {
+Route::middleware(['auth.custom', 'api'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
