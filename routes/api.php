7e9672e4e7875b8f9ab3782d456bd5d6bdaa09bf
Tran Xuan Duc <xduc24122002@gmail.com>
2024-05-10 10:24:26 +0700

    add api oauth google

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('login/google', [AuthController::class, 'loginGoogle'])->name('login-google');
 Route::post('register', [AuthController::class, 'register'])->name('register');
 Route::post('send-otp', [AuthController::class, 'sendOtp'])->name('send-otp');
 Route::post('forget-password', [AuthController::class, 'forgetPassword'])->name('forget-password');
