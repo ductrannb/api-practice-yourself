6edd9f10776cd749d57a543444aae14031601510
Tran Xuan Duc <xduc24122002@gmail.com>
2024-05-16 11:05:16 +0700

    api chart teacher

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\DashboardAdminResource;
+use App\Http\Resources\DashboardTeacherResource;
 use App\Models\CourseUser;
 use App\Models\Exam;
 use App\Models\ExamUser;
@@ -17,24 +18,24 @@ class DashboardController extends Controller
     private const TYPE_WEEK = 2;
     private const TYPE_MONTH = 3;
     private const DAY_LIMIT = 30;
-    private const WEEK_LIMIT = 8;
+    private const WEEK_LIMIT = 12;
     private const MONTH_LIMIT = 12;
 
     public function dashboardAdmin(Request $request)
     {
         $type = $request->type ?: self::TYPE_WEEK;
-        $userList = User::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d'))->get();
-        $questionList = Question::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d'))->get();
-        $examList = Exam::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d'))->get();
+        $userList = User::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d H:i:s'))->get();
+        $questionList = Question::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d H:i:s'))->get();
+        $examList = Exam::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d H:i:s'))->get();
         $courseUserList = CourseUser::where('type', CourseUser::TYPE_USER)
             ->when($type == self::TYPE_DAY, function ($query) {
-                return $query->where('created_at', '>=', Carbon::today()->subDays(self::DAY_LIMIT)->startOfYear()->format('Y-m-d'));
+                return $query->where('created_at', '>=', Carbon::today()->subDays(self::DAY_LIMIT)->startOfYear()->format('Y-m-d H:i:s'));
             })
             ->when($type == self::TYPE_WEEK, function ($query) {
-                return $query->where('created_at', '>=', Carbon::today()->subWeeks(self::WEEK_LIMIT)->startOfYear()->format('Y-m-d'));
+                return $query->where('created_at', '>=', Carbon::today()->subWeeks(self::WEEK_LIMIT)->startOfYear()->format('Y-m-d H:i:s'));
             })
             ->when($type == self::TYPE_MONTH, function ($query) {
-                return $query->where('created_at', '>=', Carbon::today()->subMonths(self::MONTH_LIMIT)->startOfYear()->format('Y-m-d'));
+                return $query->where('created_at', '>=', Carbon::today()->subMonths(self::MONTH_LIMIT)->startOfYear()->format('Y-m-d H:i:s'));
             })
             ->get();
 
@@ -62,24 +63,22 @@ class DashboardController extends Controller
     public function dashboardTeacher(Request $request)
     {
         $type = $request->type ?: self::TYPE_WEEK;
-        $questionList = Question::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d'))
+        $questionList = Question::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d H:i:s'))
             ->where('user_id', auth()->id())
             ->get();
-        $examList = Exam::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d'))
+        $examList = Exam::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d H:i:s'))
             ->where('user_id', auth()->id())->get();
-        $examUserList = ExamUser::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d'))
+        $examUserList = ExamUser::where('created_at', '>=', Carbon::today()->subYears()->startOfYear()->format('Y-m-d H:i:s'))
             ->whereHas('exam', function ($query) {
                 return $query->where('user_id', auth()->id());
             })->get();
+
         $mainChart = (object) [
             'labels' => $this->getLabels($type),
-            'joiner_data' => array_reverse(array_map(
-                fn($i) => rand(100, 1000),
-                range(0, ($type == self::TYPE_DAY ? self::DAY_LIMIT : ($type == self::TYPE_WEEK ? self::WEEK_LIMIT : self::MONTH_LIMIT) - 1))
-            )),
-            'submit_time_data' => $this->getMainChartData($courseUserList, $type),
+            'joiner_data' => $this->getMainChartData($examUserList->unique('user_id'), $type),
+            'submit_time_data' => $this->getMainChartData($examUserList, $type),
         ];
-        return $this->responseOk(data: new DashboardAdminResource((object) [
+        return $this->responseOk(data: new DashboardTeacherResource((object) [
             'questionChart' => $this->getMiniChartData($questionList),
             'examChart' => $this->getMiniChartData($examList),
             'mainChart' => $mainChart
@@ -117,46 +116,47 @@ class DashboardController extends Controller
 
     private function getMainChartData($courseUserList, $type = self::TYPE_WEEK): array
     {
+        $data = [];
         if ($type == self::TYPE_DAY) {
-            return array_reverse(array_map(
+            $data = array_map(
                 fn($i) => $courseUserList
-                    ->where('created_at', '>=', Carbon::today()->subDays($i)->startOfDay()->format('Y-m-d'))
-                    ->where('created_at', '<=', Carbon::today()->subDays($i)->endOfDay()->format('Y-m-d'))
+                    ->where('created_at', '>=', Carbon::today()->subDays($i)->startOfDay()->format('Y-m-d H:i:s H:i:s'))
+                    ->where('created_at', '<=', Carbon::today()->subDays($i)->endOfDay()->format('Y-m-d H:i:s H:i:s'))
                     ->count(),
                 range(0, self::DAY_LIMIT - 1)
-            ));
+            );
         }
         if ($type == self::TYPE_WEEK) {
-            return array_reverse(array_map(
+            $data = array_map(
                 fn($i) => $courseUserList
-                    ->where('created_at', '>=', Carbon::today()->subWeeks($i)->startOfWeek()->format('Y-m-d'))
-                    ->where('created_at', '<=', Carbon::today()->subWeeks($i)->endOfWeek()->format('Y-m-d'))
+                    ->where('created_at', '>=', Carbon::today()->subWeeks($i)->startOfWeek()->format('Y-m-d H:i:s'))
+                    ->where('created_at', '<=', Carbon::today()->subWeeks($i)->endOfWeek()->format('Y-m-d H:i:s'))
                     ->count(),
                 range(0, self::WEEK_LIMIT - 1)
-            ));
+            );
         }
         if ($type == self::TYPE_MONTH) {
-            return array_reverse(array_map(
+            $data = array_map(
                 fn($i) => $courseUserList
-                    ->where('created_at', '>=', Carbon::today()->subMonths($i)->startOfMonth()->format('Y-m-d'))
-                    ->where('created_at', '<=', Carbon::today()->subMonths($i)->endOfMonth()->format('Y-m-d'))
+                    ->where('created_at', '>=', Carbon::today()->subMonths($i)->startOfMonth()->format('Y-m-d H:i:s'))
+                    ->where('created_at', '<=', Carbon::today()->subMonths($i)->endOfMonth()->format('Y-m-d H:i:s'))
                     ->count(),
                 range(0, self::MONTH_LIMIT - 1)
-            ));
+            );
         }
-        return [];
+        return array_reverse($data);
     }
 
     private function getMiniChartData($list, $role = null)
     {
         $countThisWeek = $list
             ->when($role, fn($collection) => $collection->where('role_id', '=', $role))
-            ->where('created_at', '>=', Carbon::today()->subWeeks()->format('Y-m-d'))
+            ->where('created_at', '>=', Carbon::today()->subWeeks()->format('Y-m-d H:i:s'))
             ->count();
         $countLastWeek = $list
             ->when($role, fn($collection) => $collection->where('role_id', '=', $role))
-            ->where('created_at', '>=', Carbon::today()->subWeeks()->startOfMonth()->format('Y-m-d'))
-            ->where('created_at', '<=', Carbon::today()->subWeeks()->endOfMonth()->format('Y-m-d'))
+            ->where('created_at', '>=', Carbon::today()->subWeeks()->startOfWeek()->format('Y-m-d H:i:s'))
+            ->where('created_at', '<=', Carbon::today()->subWeeks()->endOfWeek()->format('Y-m-d H:i:s'))
             ->count();
         return (object) [
             'value' => $countThisWeek,
@@ -166,8 +166,8 @@ class DashboardController extends Controller
                 function ($i) use ($list, $role) {
                     return $list
                         ->when($role, fn($collection) => $collection->where('role_id', '=', $role))
-                        ->where('created_at', '>=', Carbon::today()->subWeeks($i)->startOfWeek()->format('Y-m-d'))
-                        ->where('created_at', '<=', Carbon::today()->subWeeks($i)->endOfWeek()->format('Y-m-d'))
+                        ->where('created_at', '>=', Carbon::today()->subWeeks($i)->startOfWeek()->format('Y-m-d H:i:s'))
+                        ->where('created_at', '<=', Carbon::today()->subWeeks($i)->endOfWeek()->format('Y-m-d H:i:s'))
                         ->count();
                 },
                 range(0, self::WEEK_LIMIT - 1)
