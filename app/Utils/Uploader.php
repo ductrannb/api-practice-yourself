df04fdf4f1f236c3da06c387a9299a94d7a8bc3c
Tran Xuan Duc <xduc24122002@gmail.com>
2024-05-22 11:43:10 +0700

    rollback upload

--- app/Utils/Uploader.php
+++ app/Utils/Uploader.php
@@ -2,6 +2,7 @@
 
 namespace App\Utils;
 
+use App\Http\Requests\UploadFileRequest;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
@@ -18,8 +19,12 @@ trait Uploader
         return $url;
     }
 
-    public function uploadFile($file)
+    public function uploadFile(UploadFileRequest $request)
     {
+        if (!$request->hasFile('file')) {
+            return response()->json(['message' => 'File not found'], Response::HTTP_NOT_FOUND);
+        }
+        $file = $request->file('file');
         if (app()->isLocal()) {
             $response = Http::withHeader('api-practice-key', env('PRACTICE_API_KEY'))->post('https://dev-api.ductran.site/api/upload-file', [
                 'file' => $file
@@ -29,7 +34,9 @@ trait Uploader
             }
             return $response->json()['url'];
         }
-
+        if ($request->header('api-practice-key') != env('PRACTICE_API_KEY')) {
+            return response()->json(['message' => 'You do not have access'], Response::HTTP_FORBIDDEN);
+        }
         $path = $this->storeFile($file);
         return response()->json([ 'url' => Storage::url($path)]);
     }
